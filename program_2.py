75b35eed9f8ee181b003cced489b76ec4b6b3760
JadonAnderstrom <jjanderstrom@students.unwsp.edu>
2024-09-10 08:48:59 -0500

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -1,13 +1,24 @@
 
 def average_age():
     # Get User Input
-
+print('Hello, user! My name is Steve and my job is to find the average age of a group of people.\n')
+print('Input age of person 1: ')
+person1 = int(input())
+print('Input age of person 2: ')
+person2 = int(input())
+print('Input age of person 3: ')
+person3 = int(input())
+print('Input age of person 4: ')
+person4 = int(input())
+print('Input age of person 5: ')
+person5 = int(input())
 
     # Sum ages
+result = (person1 + person2 + person3 + person4 + person5)
 
     # Average the ages
-
+average = (result/5)
     # Print the results
-
+print('The average age of your age group equals ' (average))
 # Line which calls the above function.
 average_age()
