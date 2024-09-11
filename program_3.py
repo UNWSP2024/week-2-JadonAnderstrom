5f945bcb1fff5bdbb75d9fc5eb0cd8d0d72ba9ab
JadonAnderstrom <jjanderstrom@students.unwsp.edu>
2024-09-11 18:00:49 -0500

    Update program_3.py

--- program_3.py
+++ program_3.py
@@ -1,5 +1,17 @@
 def calculate_total_purchase():
     # A customer in a store is purchasing five items.  
+    print('Hello, and welcome to the Anderstrom Bakery. This is a self automated checkout. Please enter the price of your items below.\n')
+    print('Insert dollar amount of item 1:')
+    item1 = input()
+    print('Insert dollar amount of item 2:')
+    item2 = input()
+    print('Insert dollar amount of item 3:')
+    item3 = input()
+    print('Insert dollar amount of item 4:')
+    item4 = input()
+    print('Insert dollar amount of item 5:')
+    item5 = input()
+    
     # Write a program that asks for each item, 
     # then displays the subtotal of the sale, 
     # the amount of sales tax, and the total.  
